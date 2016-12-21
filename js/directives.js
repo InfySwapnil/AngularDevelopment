1b83303875881a014ec4d4f1ff45e85168abaa36
Swapnil <swapkanse@gmail.com>
2016-12-21 17:22:10 +0530

    Final

--- js/directives.js
+++ js/directives.js
@@ -1,15 +1,37 @@
-angular.module("mainApp.directives",[]);
+angular.module("mainApp.directives",[])
+
 
 /* .directive("digitsOnly", function(){
 
 	return{
 		restrict: "A",
-		template: "TestData: {{Test}}"
-		/* link: function(scope, elem, attrs){
+		link: function(scope, element, attrs){
+			element[0].addEventListener("change", function(){
+				
+				if(isNaN(element[0].value))
+				console.log("This is not a number");
+				else
+				console.log("This is number");
+			});
+			
+		}
+	} */
+	
+	
+	.directive("onlyDigitsTag",function(){
+	
+		return{
 		
+			restrict: "E",
+			controller:function($scope){
+				$scope.x="New scope variable";
+			},
+			template: "<input type='text' ng-model='x'>",
+			link: function($scope, element, attrs, controller){
+				console.log($scope.x);
 		
-		} 
-	}; 
-})
-
-*/
+				
+			}
+		}
+	
+	})
